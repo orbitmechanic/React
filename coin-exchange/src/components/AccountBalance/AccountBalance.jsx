e3b40af84e453694ec18bc58e15cd9028fa57599
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-09-02 14:20:32 -0700

    Styling Account Balance with CSS

--- coin-exchange/src/components/AccountBalance/AccountBalance.jsx
+++ coin-exchange/src/components/AccountBalance/AccountBalance.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import './AccountBalance.css';
 import PropTypes from 'prop-types';
 
 export default class AccountBalance extends Component {
     render() {
         return (
-            <>
+            <div className='account-balance'>
                 ${this.props.amount}
-            </>
+            </div>
         );
     }
 }
