382d6646fbb10b7ecf9e6b6919557f027d931c2a
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-09-02 21:59:39 -0700

    Styling components solution.

--- coin-exchange/src/components/Coin/Coin.jsx
+++ coin-exchange/src/components/Coin/Coin.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import './Coin.css';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TableData = styled.td`
+    border: 1px solid #cccccc;
+    width:25vh;
+`;
 
 export default class Coin extends Component {
     constructor(props){
@@ -37,15 +42,15 @@ export default class Coin extends Component {
    }
     render() {
         return (
-            <tr className = 'coin-row'>
-                <td>{this.props.name}</td>
-                <td>{this.props.ticker}</td>
-                <td>${this.state.price}</td>
-                <td>
+            <tr>
+                <TableData>{this.props.name}</TableData>
+                <TableData>{this.props.ticker}</TableData>
+                <TableData>${this.state.price}</TableData>
+                <TableData>
                     <form action="#" method="POST">
                         <button onClick={this.handleClick}>Refresh</button>
                     </form>
-                </td>
+                </TableData>
             </tr>
         );
     }
