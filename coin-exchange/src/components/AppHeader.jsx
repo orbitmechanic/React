13c8e1e4117aefd5525e1a8ff03b027e0146276e
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-09-03 22:18:27 -0700

    Adding prop-types checking

--- coin-exchange/src/components/AppHeader.jsx
+++ coin-exchange/src/components/AppHeader.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
+import PropTypes from 'prop-types';
 
 export default class AppHeader extends Component {
     constructor(props){
@@ -13,7 +14,11 @@ export default class AppHeader extends Component {
             <header className = 'App-header'>
                 <img src = {logo} alt='React logo' className = 'App-logo'/>
                 <h1 className='App-Title'>{this.state.apName}</h1>
-          </header>
+            </header>
         )
     }
+}
+
+AppHeader.propTypes = {
+    apName: PropTypes.string.isRequired,
 }
