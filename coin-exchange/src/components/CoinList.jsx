590dc6c74a962be05299785466a2e926adaa862f
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-09-16 18:15:09 -0700

    Functionalized account balance and coin list.
    Per instructions: remove component, switch class to function, insert (props) for extends, remove render(), remove this.

--- coin-exchange/src/components/CoinList.jsx
+++ coin-exchange/src/components/CoinList.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Coin from './Coin';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
@@ -9,36 +9,36 @@ const Table = styled.table`
     font-size: 1.4rem;
 `
 
-export default class CoinList extends Component {
-    render() {
-        let columnLabel = this.props.showBalance ? <th>Balance</th> : null;
-        return (
-            <Table>
-                <thead>
-                    <tr>
-                    <th>Name</th>
-                    <th>Ticker</th>
-                    {columnLabel}
-                    <th>Price</th>
-                    <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.props.coinData.map( ({key, name, ticker, balance, price}) => 
-                            <Coin key = {key} 
-                                handleRefresh={this.props.handleRefresh}
-                                name={name}
-                                ticker={ticker}
-                                balance={balance}
-                                showBalance={this.props.showBalance}
-                                price={price} 
-                                tickerId={key} />
-                        )
-                    }
-                </tbody>
-            </Table>
-        )
-    }
+export default function CoinList (props){
+    
+    let columnLabel = props.showBalance ? <th>Balance</th> : null;
+    
+    return (
+        <Table>
+            <thead>
+                <tr>
+                <th>Name</th>
+                <th>Ticker</th>
+                {columnLabel}
+                <th>Price</th>
+                <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.coinData.map( ({key, name, ticker, balance, price}) => 
+                        <Coin key = {key} 
+                            handleRefresh={props.handleRefresh}
+                            name={name}
+                            ticker={ticker}
+                            balance={balance}
+                            showBalance={props.showBalance}
+                            price={price} 
+                            tickerId={key} />
+                    )
+                }
+            </tbody>
+        </Table>
+    )
 }
 
 CoinList.propTypes = {
