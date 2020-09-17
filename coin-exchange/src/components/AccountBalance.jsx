f5f7994047aedd916af56e63b14f890f8f1b6fa2
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-09-17 09:44:32 -0700

    Following final styling lecture.
    Added helicopter, buy/sell buttons.  Added styling to everything.

--- coin-exchange/src/components/AccountBalance.jsx
+++ coin-exchange/src/components/AccountBalance.jsx
@@ -2,22 +2,54 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const Balance = styled.div`
+    min-width: 250px;
+    margin: 0.5rem 0 0 2.5rem;
+    font-size: 1.5em;
+    vertical-align: middle;
+    text-align: center;
+`;
 const Section = styled.section`
     font-size: 2rem;
-    text-align: left;
-    padding: 1.5rem 0 1.5rem 5rem;
+    text-align: center;
+    margin-bottom:2rem;
+    line-height:3rem;
 `;
+
+const Button = styled.button`
+    margin: 0 8px;
+`;
+
+const BalanceToggleButton = styled(Button)`
+    width: 150px;
+`;
+
+var formatter = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
 
 export default function AccountBalance (props) {
     
     let buttonText = props.showBalance ? 'Hide Balance' : 'Show Balance';
-    let content = props.showBalance ? <>Balance: ${props.amount}</> : null;
+    let content = props.showBalance ? <>{formatter.format(props.amount)}</> : '\u00A0';
+    const buttonClass = 'btn ' + (props.showBalance ? 'btn-warning' : 'btn-info');
 
     return (
-        <Section>
-            {content}
-            <button onClick={props.toggleBalanceVisibility}>{buttonText}</button>
-        </Section>
+        <>
+            <Balance>{content}</Balance>
+            <Section>
+                <BalanceToggleButton 
+                    className={buttonClass} 
+                    onClick={props.toggleBalanceVisibility}>
+                    {buttonText}
+                </BalanceToggleButton>
+                <Button className='btn btn-success'
+                        onClick={props.handleBrrrr}>
+                    <i className='fas fa-helicopter'></i>
+                </Button>
+            </Section>
+        </>
     );
 }
 
