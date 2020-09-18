b4728525e587866495bd4877cdd543a01d7d26a0
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-09-18 13:30:10 -0700

    Adding a quantity prompt to trade buttons
    Simple prompt.

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -74,19 +74,17 @@ function App (props) {
   }
 
   const handleTransaction = ( isBuy, valueChangeID) => {
-    var balanceChange = isBuy ? 1: -1;
+    var balanceChange = window.prompt('How much?') * (isBuy ? 1: -1);
     const newCoinData = coinData.map( function(values) {
       let newValues = {...values};
       if ( valueChangeID === values.key) {
         if ( isBuy ) {
-          if ( balance < newValues.price ) {
-            // insufficient funds
+          if ( balance < newValues.price*balanceChange ) {
             alert('Insufficient funds for this purchase.');
             return values;
           } 
         } else {
           if (values.balance + balanceChange < 0) {
-            // insufficient holdings
             alert('Insufficient holdings for this sell order.');
             return values;
           }
