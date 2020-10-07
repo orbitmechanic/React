04701611c0ea7bc3e57389e0baa4c7c1e3985911
Orbit Mechanic <orbitmechanic@protonmail.com>
2020-10-07 12:20:26 -0700

    Updates

---
+++ precache-manifest.38b9a13a5060037a4ef59e465c5f09e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17181b1b619d1d6e5c45e9f3cfe754e0",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "546d7ebb0592e9ea6413",
+    "url": "/coin-exchange/static/css/2.75b4f570.chunk.css"
+  },
+  {
+    "revision": "166718900a1e57b79a16",
+    "url": "/coin-exchange/static/css/main.555a9668.chunk.css"
+  },
+  {
+    "revision": "546d7ebb0592e9ea6413",
+    "url": "/coin-exchange/static/js/2.0c61f49b.chunk.js"
+  },
+  {
+    "revision": "1b9f3bf752a877bd1a04dbbaede1ef59",
+    "url": "/coin-exchange/static/js/2.0c61f49b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166718900a1e57b79a16",
+    "url": "/coin-exchange/static/js/main.bf0f8004.chunk.js"
+  },
+  {
+    "revision": "c9fe44687b1447b49608",
+    "url": "/coin-exchange/static/js/runtime-main.ea956e0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-exchange/static/media/logo.5d5d9eef.svg"
+  }
+]);
